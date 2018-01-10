019c6869b279ce06da1ee233a5e1c29917ff2532
Seth Kurkowski <sckurkowski@student.fullsail.edu>
2018-01-10 14:04:15 -0500

    Accessed Current Location
    Added basic ui to fist screen. Ask user authorization for core location for either only when in use or always. Obtained user's currently location and placed it in the mapView on viewController.

--- KurkowskiSeth_Parkaholic_DVP4/KurkowskiSeth_Parkaholic_DVP4/ViewController.swift
+++ KurkowskiSeth_Parkaholic_DVP4/KurkowskiSeth_Parkaholic_DVP4/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Core Location Properties
+    let coreLocationManager = CLLocationManager()
+    var recentLocation: CLLocation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set locationManager's Delegate
+        coreLocationManager.delegate = self
+        
+        requestAuthorization()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Function to check authorization code and request authorization if it hasn't ben requested yet
+    func requestAuthorization() {
+        
+        let authorizationCode = CLLocationManager.authorizationStatus()
+        
+        if authorizationCode == CLAuthorizationStatus.notDetermined && coreLocationManager.responds(to: #selector(CLLocationManager.requestWhenInUseAuthorization)) || coreLocationManager.responds(to: #selector(CLLocationManager.requestAlwaysAuthorization)) {
+            if Bundle.main.object(forInfoDictionaryKey: "NSLocationAlwaysUsageDescription") != nil {
+                coreLocationManager.requestAlwaysAuthorization()
+            }
+        } else {
+            //Start obtaining the user's location if they have already given permision to do so.
+            coreLocationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.notDetermined || status != CLAuthorizationStatus.denied || status != CLAuthorizationStatus.restricted {
+            coreLocationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let newestLocation = locations.last {
+            recentLocation = newestLocation
+            updateMap(location: recentLocation)
+        }
+    }
+    
+    func updateMap(location: CLLocation) {
+        //Region of mapView
+        mapView.setRegion(MKCoordinateRegionMake(CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude), MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        
+        //create annotation on mapView
+        let locationPinCoord = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationPinCoord
+        
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
 
 }
 
